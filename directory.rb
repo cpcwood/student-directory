78eee57fb6d04bd81f0938a0a6dd61ad80d6ac11
cpcwood <cpcwood@outlook.com>
2019-12-02 11:51:56 +0000

    Students names indexed

--- directory.rb
+++ directory.rb
@@ -20,8 +20,8 @@ def print_header
 end
 
 def print_names(students)
-  students.each { |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index { |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
   }
   puts "-------------"
 end
