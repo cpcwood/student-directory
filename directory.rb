cd58cfb93793298a0c9a3ac20410701f3d219858
cpcwood <cpcwood@outlook.com>
2019-12-02 12:14:16 +0000

    Ages added to input on name

--- directory.rb
+++ directory.rb
@@ -1,14 +1,23 @@
 # Ask user for input
 def input_students
-  puts "Please enter the names of the students for the November Cohort"
+  puts "Please enter the names and ages of the students for the November Cohort"
   puts "To finish, just hit return twice"
+  puts "Please enter first name..."
   students = []
   name = gets.chomp
+  unless name.empty?
+    puts "Please enter age of student"
+    age = gets.chomp
+  end
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, age: age}
     puts "Now we have #{students.count} students"
     puts "Please enter next name..."
     name = gets.chomp
+    unless name.empty?
+      puts "Please enter age of student"
+      age = gets.chomp
+    end
   end
   return students
 end
@@ -22,7 +31,7 @@ end
 def print_names(students)
   i = 0
   while i < students.length
-    puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
+    puts "#{i + 1}. #{students[i][:name]}, Age: #{students[i][:age]} (#{students[i][:cohort]} cohort)"
     i += 1
   end
   puts "-------------"
